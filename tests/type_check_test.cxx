c3bf32fb6d8ead0a4b0ec3b5ab52268734f4cd45
Kai Gibson <kai@Admins-MacBook-Pro.local>
2026-08-22 01:18:34 +1000

    Type check tests

--- tests/type_check_test.cxx
+++ tests/type_check_test.cxx
@@ -21,3 +21,39 @@ TEST(TypeCheckTest, InfersInt32FromLiteralRet) {
   ASSERT_TRUE(stmt->expr_type.has_value());
   ASSERT_EQ(stmt->expr_type->type_id, TypeId::Int32);
 }
+
+TEST(TypeCheckTest, InfersInt32FromLiteralShow) {
+  CHECK_EXPR(root, "show 12");
+
+  auto stmt = cast<ShowStatement>(root.get());
+
+  ASSERT_TRUE(stmt->expr_type.has_value());
+  ASSERT_EQ(stmt->expr_type->type_id, TypeId::Int32);
+}
+
+TEST(TypeCheckTest, InfersFloat64FromLiteralDecl) {
+  CHECK_EXPR(root, "x = 12.123");
+
+  auto stmt = cast<VariableDeclarationStatement>(root.get());
+
+  ASSERT_TRUE(stmt->declaration_type.has_value());
+  ASSERT_EQ(stmt->declaration_type->type_id, TypeId::Float64);
+}
+
+TEST(TypeCheckTest, InfersFloat64FromLiteralRet) {
+  CHECK_EXPR(root, "return 12.1234");
+
+  auto stmt = cast<ReturnStatement>(root.get());
+
+  ASSERT_TRUE(stmt->expr_type.has_value());
+  ASSERT_EQ(stmt->expr_type->type_id, TypeId::Float64);
+}
+
+TEST(TypeCheckTest, InfersFloat64FromLiteralShow) {
+  CHECK_EXPR(root, "show 12.1234");
+
+  auto stmt = cast<ShowStatement>(root.get());
+
+  ASSERT_TRUE(stmt->expr_type.has_value());
+  ASSERT_EQ(stmt->expr_type->type_id, TypeId::Float64);
+}
