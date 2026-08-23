3e9e3c8fe4da4d638eb73798f1ee3b9ab1ed13cc
Kai Gibson <kai@Admins-MacBook-Pro.local>
2026-08-23 21:37:48 +1000

    Lint exception

--- include/token_type.h
+++ include/token_type.h
@@ -103,6 +103,7 @@ constexpr auto SYMBOLS = to_static_map<std::string_view, TokenType>({
 });
 
 constexpr auto token_type_to_str(TokenType t) -> const std::string_view {
+  // NOLINTNEXTLINE(bugprone-unchecked-optional-access)
   return TOKEN_TYPE_STR.find(t)->get();
 }
 
