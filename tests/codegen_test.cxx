87f048ee95a0647070ba7330018ad7c3f1de5ed9
Kai Gibson <kai@Admins-MacBook-Pro.local>
2026-08-30 00:32:30 +1000

    Add nested loop test

--- tests/codegen_test.cxx
+++ tests/codegen_test.cxx
@@ -170,3 +170,27 @@ TEST(CodegenTest, GeneratesLoopWithCycle) {
   RUN(output, program);
   ASSERT_EQ(output, 6);
 }
+
+TEST(CodegenTest, GeneratesNestedLoop) {
+  auto program = R"(
+    func main() 
+      x = 1
+      y = 1
+
+      loop 
+        set x = x + 1
+        if x >= 5 break end
+
+        loop 
+          set y = y + 1
+          if x == y break end
+        end
+      end
+
+      return y + x
+    end
+  )";
+
+  RUN(output, program);
+  ASSERT_EQ(output, 9);
+}
